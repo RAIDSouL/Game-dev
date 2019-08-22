e12c1466a1de924507ed8036c63ed56cfdb311cf
raidsoul <thunderock01@gmail.com>
2019-08-22 11:18:23 +0700

    finish class

---
+++ Game Dev/Assets/Scenes/Scripts/SpawnBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBall : MonoBehaviour
+{
+    public GameObject myredball;
+    public GameObject myblueball;
+    float intervalTime;
+    void Start()
+    {
+        intervalTime = 1.0f;
+ 
+       	InvokeRepeating("GenerateBall",5.0f,0.2f);
+    }
+
+    IEnumerator GenerateBall(){
+ 
+       	while(true){
+            	Instantiate(myblueball,transform.position,Quaternion.identity);
+       	 	    Instantiate(myredball,transform.position,Quaternion.identity);
+       	}
+ 	}
+
+}
